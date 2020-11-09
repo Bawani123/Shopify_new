12933aff2909ad590f503bf6e85178d03c280680
Bawani <brupasinghe3@gmail.com>
2020-11-09 23:15:11 +0530

    Home Activity edited

--- app/src/main/java/com/example/shopyfy/HomeActivity.java
+++ app/src/main/java/com/example/shopyfy/HomeActivity.java
@@ -2,18 +2,26 @@ package com.example.shopyfy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.shopyfy.Model.Products;
 import com.example.shopyfy.Prevalent.Prevalent;
+import com.example.shopyfy.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
@@ -34,9 +42,9 @@ public class HomeActivity extends AppCompatActivity
 {
 
     //private AppBarConfiguration mAppBarConfiguration;
-   // private DatabaseReference ProductsRef;
-    //private RecyclerView recyclerView;
-   // RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference ProductsRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
 
 
     @Override
@@ -44,7 +52,7 @@ public class HomeActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //ProductsRef = FirebaseDatabase.getInstance().getReference().child("Products");
+        ProductsRef = FirebaseDatabase.getInstance().getReference().child("Products");
 
         //Paper.init(this);
 
@@ -72,47 +80,77 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-    }
 
-        //View headerView = navigationView.getHeaderView(0);
-        //TextView userNameTextView = headerView.findViewById(R.id.user_profile_image);
-        //CircleImageView profileImageView = headerView.findViewById(R.id.user_profile_image);
 
-        //userNameTextView.setText(Prevalent.currentOnlineUser.getName());
+        View headerView = navigationView.getHeaderView(0);
+        TextView userNameTextView = headerView.findViewById(R.id.user_profile_name);
+        CircleImageView profileImageView = headerView.findViewById(R.id.user_profile_image);
+
+        userNameTextView.setText(Prevalent.currentOnlineUser.getName());
         //Picasso.get().load(Prevalent.currentOnlineUser.getImage()).placeholder(R.drawable.profile).into(profileImageView);
 
-        //recyclerView = findViewById(R.id.recycler_menu);
-        //recyclerView.setHasFixedSize(true);
-        //layoutManager = new LinearLayoutManager(this);
-        //recyclerView.setLayoutManager(layoutManager);
+        recyclerView = findViewById(R.id.recycler_menu);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
-    //}
-    //@Override
-    //protected void onStart()
-    //{
-       // super.onStart();
+    }
 
-   //     FirebaseRecyclerOptions<Product> options =
-    //            new FirebaseRecyclerOptions.Builder<Products>()
-     //           .setQuery(ProductsRef, Products.class)
-       //         .build();
+        @Override
+        protected void onStart()
+        {
+            super.onStart();
+
+            FirebaseRecyclerOptions<Products> options =
+                    new FirebaseRecyclerOptions.Builder<Products>()
+                    .setQuery(ProductsRef, Products.class)
+                    .build();
+
+            FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
+                    new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                        @Override
+                        protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model)
+                        {
+                            holder.txtProductName.setText(model.getPname());
+                            holder.txtProductDescription.setText(model.getDescription());
+                            holder.txtProductPrice.setText("Price = " + model.getPrice() + "$");
+                            Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                        }
+
+                        @NonNull
+                        @Override
+                        public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+                        {
+                            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent, false);
+                            ProductViewHolder holder = new ProductViewHolder(view);
+                            return holder;
+
+                        }
+                    };
+
+            recyclerView.setAdapter(adapter);
+            adapter.startListening();
+        //            new FirebaseRecyclerOptions.Builder<Products>()
+        //           .setQuery(ProductsRef, Products.class)
+        //         .build();
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-       // mAppBarConfiguration = new AppBarConfiguration.Builder(
-                //R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-               // .setDrawerLayout(drawer)
-              //  .build();
+        // mAppBarConfiguration = new AppBarConfiguration.Builder(
+        //R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+        // .setDrawerLayout(drawer)
+        //  .build();
 
 
         //NavigationView navigationView1 = (NavigationView) findViewById(R.id.nav_view);
         //navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
-    //}
+        //}
 
 
         //NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         //NavigationUI.setupWithNavController(navigationView, navController);
-
+    }
 
     @Override
     public void onBackPressed() {
