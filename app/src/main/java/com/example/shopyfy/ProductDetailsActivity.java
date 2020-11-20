edeebb32adc32f09c83dff05ae2beafaed7286fa
HiruniAmandakoon <hiruniamandakoon@gmail.com>
2020-11-20 18:10:28 +0530

    confirm final order activity validations

--- app/src/main/java/com/example/shopyfy/ProductDetailsActivity.java
+++ app/src/main/java/com/example/shopyfy/ProductDetailsActivity.java
@@ -37,7 +37,7 @@ public class ProductDetailsActivity extends AppCompatActivity
     private ImageView productImage;
     private ElegantNumberButton numberButton;
     private TextView productPrice, productDescription, productName;
-    private String productID = "";
+    private String productID = "",state = "Normal";
 
 
 
@@ -63,13 +63,27 @@ public class ProductDetailsActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                addingToCartList();
+
+                if (state.equals("Order Placed") || state.equals("Order Shipped"))
+                {
+                    Toast.makeText(ProductDetailsActivity.this, "You can purchase more products, once your order is shipped or confirmed.", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    addingToCartList();
+                }
             }
-                                           }
-        );
+        });
 
     }
 
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        CheckOrderState();
+    }
 
     private void addingToCartList()
     {
@@ -154,4 +168,37 @@ public class ProductDetailsActivity extends AppCompatActivity
             }
         });
     }
+
+    private void CheckOrderState()
+    {
+        DatabaseReference ordersRef;
+        ordersRef = FirebaseDatabase.getInstance().getReference().child("Orders").child(Prevalent.currentOnlineUser.getPhone());
+
+        ordersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.exists())
+                {
+                    String shippingState = dataSnapshot.child("state").getValue().toString();
+
+                    if (shippingState.equals("shipped"))
+                    {
+                        state = "Order Shipped";
+                    }
+                    else if (shippingState.equals("not shopped"))
+                    {
+                        state = "Order Placed";
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+
+            }
+        });
+    }
+
 }
